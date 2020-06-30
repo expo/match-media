b4f0c02a6c2334e345eb8be428f25e20954a276e
Evan Bacon <baconbrix@gmail.com>
2020-06-30 14:36:26 -0700

    Clean up

---
+++ src/MediaQueryList.ts
@@ -0,0 +1,79 @@
+import { Subscription } from "@unimodules/core";
+import mediaQuery from "css-mediaquery";
+import * as ScreenOrientation from "expo-screen-orientation";
+import { Dimensions } from "react-native";
+
+type Listener = (context: MediaQueryList) => any;
+
+/**
+ * A pseudo implementation of MediaQueryList
+ * https://www.w3.org/TR/css3-mediaqueries/
+ */
+export default class MediaQueryList /* extends MediaQueryList */ {
+  private listeners: Listener[] = [];
+
+  private orientation: ScreenOrientation.Orientation =
+    ScreenOrientation.Orientation.PORTRAIT_UP;
+
+  private unsubscribe: Subscription;
+
+  constructor(private query: string) {
+    (async () => {
+      try {
+        const orientation = await ScreenOrientation.getOrientationAsync();
+        this.updateListeners({ orientation });
+      } catch (_) {}
+    })();
+
+    this.unsubscribe = ScreenOrientation.addOrientationChangeListener(
+      ({ orientationInfo: { orientation } }) => {
+        this.updateListeners({ orientation });
+      }
+    );
+
+    Dimensions.addEventListener("change", this.resize);
+  }
+
+  private resize = () => {
+    this.updateListeners({ orientation: this.orientation });
+  };
+
+  // TODO: find an automatic interface for unmounting
+  _unmount() {
+    if (this.unsubscribe) {
+      this.unsubscribe.remove();
+    }
+    Dimensions.removeEventListener("change", this.resize);
+  }
+
+  public addListener(listener: Listener) {
+    this.listeners.push(listener);
+  }
+
+  public removeListener(listener: Listener) {
+    const index = this.listeners.indexOf(listener);
+    if (index !== -1) this.listeners.splice(index, 1);
+  }
+
+  public get matches(): boolean {
+    const windowDimensions = Dimensions.get("window");
+    return mediaQuery.match(this.query, {
+      type: "screen",
+      orientation:
+        this.orientation === ScreenOrientation.Orientation.LANDSCAPE_LEFT ||
+        this.orientation === ScreenOrientation.Orientation.LANDSCAPE_RIGHT
+          ? "landscape"
+          : "portrait",
+      ...windowDimensions,
+      "device-width": windowDimensions.width,
+      "device-height": windowDimensions.height
+    });
+  }
+
+  private updateListeners({ orientation }) {
+    this.orientation = orientation;
+    this.listeners.forEach(listener => {
+      listener(this);
+    });
+  }
+}
